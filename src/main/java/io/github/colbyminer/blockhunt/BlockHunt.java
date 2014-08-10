d6758f1d6d522ef56a68c439f9f00ffd3439067f
Zack Waters <zswaters@gmail.com>
2014-08-10 01:28:12 -0700

    Initial check-in of project.

---
+++ src/main/java/io/github/colbyminer/blockhunt/BlockHunt.java
@@ -0,0 +1,16 @@
+package io.github.colbyminer.blockhunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlockHunt extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
